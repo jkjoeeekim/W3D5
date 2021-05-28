6eb162c6fb028558b5790b2b91ab61ce64c16021
Joseph Kim <jkjoeeekim@live.com>
2021-05-28 16:38:14 -0700

    Add new_move_positions

--- knights_travails_project/knight_path_finder.rb
+++ knights_travails_project/knight_path_finder.rb
@@ -1,40 +1,49 @@
-require_relative './poly_tree_node.rb'
+require_relative './poly_tree_node'
 
 class KnightPathFinder
-  attr_reader :start_pos, :root_node
-  
   def self.valid_moves(pos)
     possible_moves = []
 
-    # pos.each do |horizontal|
-    #   pos.each do |vertical|
-        
-    #   end
-    # end
-
     (0..7).each do |horizontal_movement|
       if pos[0] + 1 == horizontal_movement || pos[0] - 1 == horizontal_movement
-        vert_move_1 = pos[1] + 2
-        vert_move_2 = pos[1] - 2
-        possible_moves << [horizontal_movement, vert_move_1] if (0..7).include?(vert_move_1)
-        possible_moves << [horizontal_movement, vert_move_2] if (0..7).include?(vert_move_2)
+        vert_move1 = pos[1] + 2
+        vert_move2 = pos[1] - 2
+        possible_moves << PolyTreeNode.new([horizontal_movement, vert_move1]) if (0..7).include?(vert_move1)
+        possible_moves << PolyTreeNode.new([horizontal_movement, vert_move2]) if (0..7).include?(vert_move2)
       elsif pos[0] + 2 == horizontal_movement || pos[0] - 2 == horizontal_movement
-        vert_move_1 = pos[1] + 1
-        vert_move_2 = pos[1] - 1
-        possible_moves << [horizontal_movement, vert_move_1] if (0..7).include?(vert_move_1)
-        possible_moves << [horizontal_movement, vert_move_2] if (0..7).include?(vert_move_2)
+        vert_move1 = pos[1] + 1
+        vert_move2 = pos[1] - 1
+        possible_moves << PolyTreeNode.new([horizontal_movement, vert_move1]) if (0..7).include?(vert_move1)
+        possible_moves << PolyTreeNode.new([horizontal_movement, vert_move2]) if (0..7).include?(vert_move2)
       end
     end
 
     possible_moves
   end
 
+  attr_reader :start_pos, :root_node
+  attr_accessor :considered_positions
+
   def initialize(pos)
     @root_node = PolyTreeNode.new(pos)
     @start_pos = root_node
+    @considered_positions = [start_pos]
+  end
+
+  def new_move_positions(pos)
+    possible_moves = KnightPathFinder.valid_moves(pos)
+
+    possible_moves.select! do |move|
+      considered_positions.none? do |prev_pos|
+        prev_pos.value == move.value
+      end
+    end
+
+    considered_positions.concat(possible_moves)
+    possible_moves
   end
 
   def build_move_tree
-
+    
   end
 end
