5cb0e1faae7cbe2dcb09b67f8bd8fa6d64048b2b
Joseph Kim <jkjoeeekim@live.com>
2021-05-28 12:20:22 -0700

    Add initialize and parent= methods

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,17 @@
 class PolyTreeNode
+  attr_reader :value, :parent, :children
 
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(node)
+    unless self.parent.nil?
+      self.parent.children.delete(self)
+    end
+    @parent = node
+    node.children << self unless node.nil? || node.children.include?(self)
+  end
 end
